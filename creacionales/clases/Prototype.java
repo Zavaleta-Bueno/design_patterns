06a68aa7c4217db59a17c36f8598af5152507edd
Zavaleta-Bueno <romelzavaleta@uni.pe>
2020-08-24 09:59:48 -0500

    Prototype

---
+++ creacionales/clases/Prototype.java
@@ -0,0 +1,18 @@
+package creacionales.clases;
+
+
+import javax.swing.JFrame;
+
+public class Prototype {
+
+    public static  void main(String[] args) {
+        System.out.println("EE");
+        /*
+        Marco marco = new Marco();
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marco.setLocationRelativeTo(null);
+        marco.setResizable(false);
+        marco.setVisible(true);*/
+    }
+    
+}
